5c6275b7463a330745b35d0d1c9116608193d5eb
Anirudhnegi09 <negianirudh385@gmail.com>
2026-06-12 18:45:29 +0530

    Add files via upload

---
+++ my_profile.py
@@ -0,0 +1,20 @@
+# Program: My Profile
+# Author: Anirudh Negi
+
+import math
+import random
+import datetime
+
+# Personal details
+print("Name: Anirudh Negi")
+print("Year: 2nd")
+print("College: CGC University")
+
+# Square root of my age
+print("Square root of my age:", math.sqrt(19))
+
+# Random number between 1 and 100
+print("Random number:", random.randint(1, 100))
+
+# Today's date
+print("Today's date:", datetime.date.today())
